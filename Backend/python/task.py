39d6c8629cc1ced0fa4b37f6745836899816efd6
nithish <pnithish692@gmail.com>
2024-09-12 17:19:33 +0530

    pushed

---
+++ Backend/python/task.py
@@ -0,0 +1,117 @@
+# 1.Print all data types in one page
+
+# lst=['str','int','float','complex','list','tuple','set','dict','bool']
+# for i in lst:
+#     print(i)
+
+
+
+
+# 2.Round of the decimal 10.356
+
+# a=10.356
+# print(round(a))
+
+
+# Find add or even numbers - [12,45,8,0,3,20,11,4,6,5]
+
+# lst=[12,45,8,0,3,20,11,4,6,5]
+# lst1=[]
+# lst2=[]
+# lst3=[]
+# for i in lst:
+#     if i%2==0:
+#         lst1.append(i)
+#         print("even number")
+#     if i%3==0:
+#         lst2.append(i)    
+#         print("odd number")
+#     else:
+#        lst3.append(i)
+#        print("remaining numbers")
+# print(lst1)
+# print(lst2)
+# print(lst3)
+
+
+# 3.Replace String Hello World to Hello John
+
+
+# str1="hello world"
+# c= str1.replace("world","jhon")
+# print(c)
+
+
+
+
+
+# 4.REVERSE A STRING 
+
+# str=input('Enter any text:')
+# print(str[::-1])
+
+
+
+
+
+
+# REVERSE A STRING 
+
+# str=input('Enter any text:')
+# print(str[::-1])
+
+
+
+# 5.Find add or even numbers - [12,45,8,0,3,20,11,4,6,5]
+
+# lst=[12,45,8,0,3,20,11,4,6,5]
+# lst1=[]
+# lst2=[]
+# lst3=[]
+# for i in lst:
+#     if i%2==0:
+#         lst1.append(i)
+#         print("even number")
+#     if i%3==0:
+#         lst2.append(i)    
+#         print("odd number")
+#     else:
+#        lst3.append(i)
+#        print("remaining numbers")
+# print(lst1)
+# print(lst2)
+# print(lst3)
+
+
+
+# 6.Sum of Tuples
+
+# tuple1 = [(4, 5),(1, 5),(6, 12)]
+# sum2= []
+# for i in tuple1:
+#     sum2.append(sum(i))
+# print(sum2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
